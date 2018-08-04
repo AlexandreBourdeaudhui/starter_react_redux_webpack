f024e80703c8aae060b53d24b2fe2e5d2e974fd5
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2018-08-04 03:46:51 +0200

    Precommit is now running - add Husky, lint-staged

--- tests/.setup.js
+++ tests/.setup.js
@@ -2,28 +2,37 @@
  * Package Import
  */
 var path = require('path');
-var babelRegister = require('babel-register');
-var babelResolver = require('babel-resolver');
 var JSDOM = require('jsdom').JSDOM;
 
+// Enzyme
+var Enzyme = require('enzyme');
+var Adapter = require('enzyme-adapter-react-16');
+
+// Babel
+var babelRegister = require('babel-register');
 
 /*
  * Babel
  */
-const app = path.join(__dirname, '..', 'app');
-const resolveModuleSource = babelResolver(app);
-babelRegister({ resolveModuleSource });
+babelRegister();
 
+/*
+ * Enzyme
+ */
+Enzyme.configure({ adapter: new Adapter() });
 
 /*
  * Global stuff
  */
 var exposedProperties = ['window', 'navigator', 'document'];
-var js = new JSDOM();
+var js = new JSDOM('', { url: 'http://localhost/' });
 global.window = js.window;
 global.document = js.window.document;
 global.HTMLElement = js.window.HTMLElement;
-Object.keys(document.defaultView).forEach((property) => {
+global.navigator = {
+  userAgent: 'node.js',
+};
+Object.keys(document.defaultView).forEach(property => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
     global[property] = document.defaultView[property];
